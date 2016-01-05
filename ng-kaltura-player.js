c1225d06c89fabe6213a9c24ed1be44b42081e2a
Manuel Rodriguez <rodriguez.manu@gmail.com>
2016-01-05 19:50:04 -0300

    Add provider, change styling ans some improvements

--- ng-kaltura-player.js
+++ ng-kaltura-player.js
@@ -1,59 +1,73 @@
-var kalturaDirectivesModule = angular.module('Kaltura.directives', []);
+(function() {
+    'use strict';
 
-kalturaDirectivesModule.directive('kalturaPlayer', ['$rootScope', function($rootScope) {
-	return {
+    angular
+        .module('Kaltura.directives')
+        .directive('kalturaPlayer', kalturaPlayer);
 
-		restrict: 'E',
-		template: '<div id="kaltura_player_{{id}}" style="width:{{width}}; height:{{height}}; background-color: black"></div>',
-		scope: {},
+    kalturaPlayer.$inject = ['$rootScope', 'kalturaPlayerConfig'];
 
-		compile: function(element, attributes) {
+    // @ngInject
+    function kalturaPlayer($rootScope, kalturaPlayerConfig) {
+        return {
+            restrict: 'E',
+            template: '<div id="kaltura_player_{{id}}" style="width:{{width}}; height:{{height}};' +
+                'background-color: black"></div>',
+            scope: {},
+            compile: compile
+        };
 
-			if (document.getElementById("kalturaLib") === null){
-				var s = document.createElement('script');
-				s.src = 'http://cdnapi.kaltura.com/p/'+attributes.pid+'/sp/'+attributes.pid+'00/embedIframeJs/uiconf_id/'+attributes.uiconfid+'/partner_id/'+attributes.pid;
-				s.id = "kalturaLib";
-				s.async = false;
-				document.head.appendChild(s);
-			}
+        function compile() {
+            var options = kalturaPlayerConfig.getOptions();
 
-			var linkFunction = function($scope, element, attributes) {
-				if (attributes.width){
-					$scope.width = attributes.width;
-				}
-				if (attributes.height){
-					$scope.height = attributes.height;
-				}
-				if (attributes.id){
-					$scope.id = attributes.id;
-				}
+            return {
+                pre: appendScript(options),
+                post: function postLink($scope, element, attributes) {
+                    if (attributes.width) {
+                        $scope.width = attributes.width;
+                    }
+                    if (attributes.height) {
+                        $scope.height = attributes.height;
+                    }
+                    if (attributes.id) {
+                        $scope.id = attributes.id;
+                    }
 
+                    if (!$scope.kdp) {
+                        var intervalID = setInterval(function() {
+                            if (typeof window.kWidget !== 'undefined') {
+                                clearInterval(intervalID);
+                                var target = attributes.id ? 'kaltura_player_' + attributes.id : 'kaltura_player_',
+                                    flashvars = attributes.flashvars ? JSON.parse(attributes.flashvars) : {};
 
-				if (!$scope.kdp){
-					var intervalID = setInterval(function(){
-						if (typeof window.kWidget !== "undefined"){
-							clearInterval(intervalID);
+                                window.kWidget.embed({
+                                    'targetId': target,
+                                    'wid': '_' + options.pid,
+                                    'uiconf_id': options.uiconfid,
+                                    'flashvars': flashvars,
+                                    'cache_st': Math.random(),
+                                    'entry_id': attributes.entryid,
+                                    'readyCallback': function(playerID) {
+                                        $scope.kdp = document.getElementById(playerID);
+                                        $rootScope.$broadcast('kalturaPlayerReady', $scope.kdp, attributes.id);
+                                    }
+                                });
+                            }
+                        },50);
+                    }
+                }
+            };
+        }
 
-							var target = attributes.id ? "kaltura_player_"+attributes.id : "kaltura_player_";
-							var flashvars = attributes.flashvars ? JSON.parse(attributes.flashvars) : {};
-
-							window.kWidget.embed({
-								"targetId": target,
-								"wid": "_"+attributes.pid,
-								"uiconf_id": attributes.uiconfid,
-								"flashvars": flashvars,
-								"cache_st": Math.random(),
-								"entry_id": attributes.entryid,
-								"readyCallback": function(playerID){
-									$scope.kdp = document.getElementById(playerID);
-									$rootScope.$broadcast('kalturaPlayerReady', $scope.kdp, attributes.id);
-								}
-							});
-						}
-					},50);
-				}
-			}
-			return linkFunction;
-		}
-	}
-}]);
+        function appendScript(options) {
+            if (document.getElementById('kalturaLib') === null) {
+                var s = document.createElement('script');
+                s.src = 'http://cdnapi.kaltura.com/p/' + options.pid + '/sp/' + options.pid +
+                    '/embedIframeJs/uiconf_id/' + options.uiconfid + '/partner_id/' + options.pid;
+                s.id = 'kalturaLib';
+                s.async = false;
+                document.head.appendChild(s);
+            }
+        }
+    }
+})();
